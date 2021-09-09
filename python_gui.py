41b45c8f890ff2c6122a740b3a606e010718099a
Eccomaggio <eccomaggio@gmail.com>
2021-09-09 21:37:20 +0800

    Initial project version

---
+++ python_gui.py
@@ -0,0 +1,22 @@
+# from https://realpython.com/python-gui-tkinter/
+import tkinter as tk
+window = tk.Tk()
+label = tk.Label(
+    text="Hello, blah",
+    fg="white",
+    bg="black",
+    width=10,
+    height=10
+    )
+label.pack()
+
+button = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+    )
+button.pack()
+
+window.mainloop()
