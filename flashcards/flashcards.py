a8169928b3e2136addf917783739dea63cf1948a
eccomaggio <eccomaggio@gmail.com>
2021-10-24 20:21:34 +0800

    fixes + pretty printing

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -75,6 +75,7 @@ def save_json_deck(filename, flashcard_deck):
     with open(filename,"w") as new_deck:
         # saved_deck = json.dump(flashcard_deck, new_deck, cls=DateTimeEncoder)
         saved_deck = json.dump(flashcard_deck, new_deck)
+        print(f"++ json file ({filename}) saved.")
 
 def retrieve_json_file(file):
     with open(file,"r") as f:
@@ -180,6 +181,7 @@ def main():
     exclude_file = Path(current_dir / "exclude_these.txt")
     json_file = Path(current_dir / "all_decks.json")
     exclude_these = populate_exclusion_list(current_dir, exclude_file)
+    user = "paul"
 
     # print (f">>>Files to exclude: {exclude_these}.")
 
@@ -189,18 +191,13 @@ def main():
     files_read = []
     for full_path in sorted(current_dir.glob("*.csv")):
         file_name = Path(full_path).name
-        # print(f"___{file_name}____")
         if file_name in exclude_these:
             # print(f"Cards in file <{file_name}> already loaded.")
             pass
         else:
             csv_decks = create_deck_from_file(full_path)
             files_read.append(file_name)
-            # print(csv_decks)
             tmp_decks = {key:val for key, val in csv_decks.items() if len(csv_decks[key]) > 0}
-            # for key in tmp_decks.keys():
-            #     print (f"{key} has {len(tmp_decks[key])} ")
-            # all_decks[new_deck[0]] = new_deck[1]
             all_decks.update(tmp_decks)
 
     ## Add newly read files to exclusion files
@@ -209,7 +206,6 @@ def main():
         with open(exclude_file, "a") as f:
             for entry in files_read:
                 f.write(f"{entry}\n")
-                # print(f"....{entry}")
 
     for deck in all_decks.keys():
         print(f"{deck} has {len(all_decks[deck])} entries.")
@@ -224,15 +220,21 @@ def main():
             "layout": {},
             "set": {},
             "skip": False,
-            "shuffle": True,
-            "review": True
+            "shuffle" : False,
+            "review": True,
+            "user": user,
             }
 
+    reset = "\u001b[0m"
+    blue = "\u001b[34m"
+    green = "\u001b[32m"
+
     ## Decide layout of flashcards for this session
-    choice = input("\n> What prompt do you want to see? "
-            "\nA: Show English first (default)"
-            "\nB: Show pinyin first"
-            "\nC: Show Chinese\n").lower()
+    query = (f"{blue}\n>\u001b[34m What prompt do you want to see? "
+            f"{green}\nA: Show English first (default)"
+            f"{green}\nB: Show pinyin first"
+            f"{green}\nC: Show Chinese\n{reset}")
+    choice = input(query).lower()
     if choice not in ["a", "b", "c"]: choice = "a"
 
     layouts = { 
@@ -250,7 +252,6 @@ def main():
     for r in range(displ_rows):
         for c in range(displ_cols):
             try:
-                # tmp_row.append(f"{i}: {decks_available[i]} ({len(decks_available[i])})")
                 tmp_row.append(f"{i}: {decks[i]} ({len(all_decks[decks[i]])})")
             except:
                 tmp_row.append("")
@@ -262,7 +263,7 @@ def main():
     f_string = "{: <30} " * displ_cols
     for r in displ_table:
         print(f_string.format(*r))
-    tmp = input("\n> Which set do you want to use? ")
+    tmp = input(f"{blue}\n> Which set do you want to use? {reset}")
     choice = 0
     try:
         choice = int(tmp)
@@ -272,61 +273,55 @@ def main():
         choice = len(decks) - 1
 
     session["set"] = all_decks[decks[choice]]
-    session["order"] = list(session["set"].keys())
 
-    
-
-    choice = input("\n> Do you want to skip marked cards? ").lower() or "y"
+    choice = input(f"{blue}\n> Do you want to skip marked cards?{reset} ").lower() or "y"
     session["skip"] = choice[0] == "y"
-    # if session["skip"] :
-    #     session["order"] = [lemma for lemma in session["order"] if session["set"][lemma]["ranking"] < 100]
 
 
-    choice = input("\n> Do you want to see the cards in a random order? ").lower() or "y"
+    choice = input(f"{blue}\n> Do you want to see the cards in a random order?{reset} ").lower() or "y"
     session["shuffle"] = choice[0] == "y"
-    # if session["shuffle"] :
-    #     random.shuffle(session["order"])
 
 
-    choice = input("\n> Do you want to review incorrectly answered cards? ").lower() or "y"
+    choice = input(f"{blue}\n> Do you want to review incorrectly answered \
+cards?{reset} ").lower() or "y"
     session["review"] = choice[0] == "y"
-
 
     ## Create the running list according to specifications
     running_list = []
     if session["skip"]:
         deck = session["set"]
         running_list = [deck[card]["lemma"] for card in deck if deck[card]["ranking"] < 100]
-        # running_list = [card["lemma"] for card in session["set"] if \
-        #         card["ranking"] < 100]
     else:
         running_list = [*session["set"].keys()]
     if session["shuffle"]:
         random.shuffle(running_list)
-    running_list_count = len(running_list) - 1
-
-    card_total = len(session["order"])
+    card_total = len(running_list)
+    running_list_count = 0
     count = 0
-    # for lemma in session["order"]:
-    while running_list_count >= 0:
+
+
+    while running_list_count < len(running_list):
         lemma = running_list[running_list_count]
-        running_list_count -= 1
         
         card = session["set"][lemma]
-        count += 1
         card["lastview"] = dt.datetime.now().isoformat()
         card["views"] += 1
 
         skip_card = hint_shown = quit_session = False
         print()
-        print(f"{count} out of {card_total} (card no.{card['seq']})")
-        print(f"prompt: {card[session['layout']['prompt']]}")
+        print(f"{blue}{running_list_count + 1} out of {len(running_list)} \
+(card no.{card['seq']}){reset}")
+        print(f"{blue}prompt:{reset} {card[session['layout']['prompt']]}")
         timing = time.perf_counter()
         while True: 
-            action = input("Select: press h(int), s(kip), r(eveal answer) or q(uit) ").lower()
+            action = input(f"{blue}Select: press h(int), s(kip), r(eveal \
+answer) or q(uit){reset} ").lower()
+            if action == "":
+                action = "r"
             if action[0] in ["h","s","r", "q"]:
                 if action[0] == "h" and hint_shown == False:
-                    print(f"(Hint: {card[session['layout']['hint']]})")
+                    print(f"{blue}(Hint: \
+                            {card[session['layout']['hint']]}){reset}")
                 elif action[0] == "s":
                     skip_card = True
                     card["skipped"] += 1
@@ -336,27 +331,38 @@ def main():
                     break
                 else:
                     timing = time.perf_counter() - timing
-                    print(f'Answer: {card["lemma"]} ({card["pinyin"]}) = {card["gloss"]}')
+                    print(f'\n{green}Answer:{reset} {card["lemma"]} ({card["pinyin"]}) = \
+{card["gloss"]}{reset}')
                     while True:
-                        choice = input("Were you correct? y(es) or n(o)? ").lower() or "y"
+                        choice = input(f"{blue}Were you correct? y/(n)?{reset} ").lower() or "y"
                         if choice[0] == "n":
                             card["wrong"] += 1
                             if session["review"]:
                                 running_list.append(lemma)
-                                running_list_count += 1
                         break
                     break
 
         if skip_card: continue    
         if quit_session: break
-        print(f"That took you: {timing:0.1f} seconds.")
+        print(f"{green}That took you: {timing:0.1f} seconds.{reset}")
 
-        save_json_deck(json_file,all_decks)
+        running_list_count += 1
 
-        # print(session)
+    save_json_deck(json_file,all_decks)
 
-    for lemma in session["set"]:
-        card = session["set"][lemma]
-        print(f'{card["lemma"]} views: {card["views"]} [wrong: {card["wrong"]}, skipped: {card["skipped"]}]')
+
+    # for lemma in session["set"]:
+    #     card = session["set"][lemma]
+    #     print(f'{card["lemma"]} views: {card["views"]} [wrong: {card["wrong"]}, skipped: {card["skipped"]}]')
+
+    report = [["word", "views", "wrong", "skipped"]]
+    deck = session["set"]
+    for key in deck.keys():
+        card = deck[key]
+        report.append([card["lemma"], card["views"], card["wrong"],
+        card["skipped"]])
+
+    for row in report:
+        print(f"{green}{row[0]:>10} {blue}{row[1]:>10} {row[2]:>10} {row[3]:>10}{reset}") 
 
 main()
