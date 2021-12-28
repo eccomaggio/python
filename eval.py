fe9e848adf3b3d92f54bab9fcedce0f5979308aa
eccomaggio <eccomaggio@gmail.com>
2021-12-28 07:50:50 +0000

    synchronized eval from main branch so that i can start tweaking it

--- eval.py
+++ eval.py
@@ -1,14 +1,9 @@
 import re
 
 # math_string = "1+246x3"
-math_input = "1 + 24.0 * 3 / 2 - 4 + 2 * 8"
-# N.B. problems with division because it currently expects integers! (so 36.0 confuses it!)
+math_input = "1.5 + 24.0 x 3 / 2 - 40 + 2 * 8"
+ALLOWED_OPS = "+-/*x"
 debug = True
-# math_array = []
-# prev = ""
-# i = -1
-# print(math_string)
-# mathArr2 = re.sub(r"(\d+|x|\+|\-|\*|\/)",r"\1 ",math_string).split()
 
 def make_array(raw_string):
     math_string = raw_string.replace(" ","")
@@ -17,35 +12,73 @@ def make_array(raw_string):
     prev = ""
     i = -1
     for count, char in enumerate(math_string):
-        pos = ""
-        # if char.isdigit():
+        el_type = ""
         if char in "0123456789.":
-            pos = "num"
-        elif char in ("+-/*x"):
-            pos = "op"
+            el_type = "NUM"
+        elif char in ALLOWED_OPS:
+            el_type = "OP"
+            ## Allow negative numbers
+            if char == "-" and (prev == "" or math_array[-1] in ALLOWED_OPS):
+                el_type = "NUM"
+        elif char in "()":
+            el_type = "BR"
         else:
-            print (f"Character position {count}: not a number or operator.")
-            break
-        if pos == prev:
-            if pos == "num":
+            print (f"Character at position {count} not a number or operator.")
+            continue
+
+        if el_type == prev and el_type != "BR":
+            if el_type == "NUM":
                 math_array[i] += char
+                if debug: print(f"{el_type} added: {char}")
             else:
-                print(f"Character pos {count}: only one operator at a time allowed.")
-            # print(f"run = ...{char} at {i}")
+                print(f"Character el_type {count}: only one operator at a time allowed.")
         else:
             math_array.append(char)
+            if debug: print(f"{el_type} added: {char}")
             i += 1
-            # print(f"this = {char} at {i}")
-            prev = pos
+        prev = el_type
+
+    if debug: print(f"first pass:\n{math_array}")
+    for i, el in enumerate(math_array):
+        if el in ALLOWED_OPS:
+            if el == "x":
+                math_array[i] = "*"
+        elif el in "()":
+            continue
+        else:
+            math_array[i] = float(el)
+
+    i = 0
+    while i < len(math_array) - 1:
+        num = math_array[i]
+        if isinstance(num,str) and num in "()":
+            i += 1 
+            continue
+        i += 1
+        while math_array[i] in "()":
+            i += 1
+        op = math_array[i]
+        assert isinstance(num,float), f"Problem here: {math_array[:i]}^"
+        assert isinstance(op,str), f"Problem here: {math_array[:i]}^"
+        i += 1
+
+    assert isinstance(math_array[-1],float), f"End with a number: {math_array}"
+
     return math_array
 
-# math_array = make_array(math_string)
-
-# print(math_array)
-# if not (len(math_array) % 3):
-#     print("Wrong number of arguments.")
-#     quit()
-
+def final_pass(arr):
+    ## Strip out brackets for now
+    basic_array = [el for el in arr if isinstance(el,float) or 
+            (isinstance(el,str) and el not in "()")]
+    if debug: print(f"basic array:\n{basic_array}")
+    for i,el in enumerate(basic_array):
+        if isinstance(el,str) and el == "-":
+           basic_array[i] = "+"
+           basic_array[i + 1] = 0 - basic_array[i + 1]
+    
+    final_result = [el for el in f_p(basic_array,[]) if isinstance(el,float)]
+    return sum(final_result)
+    # return f_p(basic_array,[])
 
 def f_p(arr,result_arr):
     if len(arr) <= 1:
@@ -58,21 +91,18 @@ def f_p(arr,result_arr):
     if op in "+-":
         result_arr += [left,op]
         if debug: print(f"addition / subtraction:\n{result_arr[2:]},\n{arr}\n")
-        # right = str(0 - int(arr[2:1]) ) if op == "-" else arr[2:1] 
-        # rest = [right] + arr[3:]
-        # return rest
         return f_p(arr[2:],result_arr)
 
     elif op in "*x/":
         right = left / float(arr[2]) if op == "/" else left * float(arr[2])
-        recurse = [str(right)] + arr[3:]
+        # recurse = [str(right)] + arr[3:]
+        recurse = [right] + arr[3:]
         if debug: print(f"division / multiplication:\n{result_arr}: ({left} {op} {int(arr[2])}) => {right}\n{recurse}\n")
         return f_p(recurse,result_arr)
 
-print(f"Raw input: {math_input}")
 math_array = make_array(math_input)
-print(f"Primary processing: {math_array}")
-print()
-result_array = f_p(math_array,[])
+if debug: print(f"Primary processing: {math_array}\n")
+result_array = final_pass(math_array)
+if debug: print(f"Raw input: {math_input}")
 print(result_array)
 
