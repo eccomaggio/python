ba3a9f5ec61de3f6723a2772e7178f3dd25dd11b
eccomaggio <eccomaggio@gmail.com>
2022-01-24 00:14:02 +0800

    working version

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -4,6 +4,7 @@ let maxCol = 5
 let currRow = 0
 let currCol = 0
 let atEOR = false
+const key = "robot"
 
 function drawWordle() {
     let inputBoxes = document.createElement("div")
@@ -45,32 +46,52 @@ function currCell() {
 }
 
 function updateWordle(id,i, feedback){
-    console.log(id,i,feedback)
+    // console.log(id,i,feedback)
     classNames = ["wrong","nearly","exact"]
     document.getElementById(id).className = classNames[feedback]
 }
 
+function replaceAt(string,i,replacement){
+    if (i >= string.length) alert("index is out of bounds")
+    const head = string.slice(0,i)
+    const tail = string.slice(i + replacement.length)
+    return head + replacement + tail
+}
+
+function compareToKey(letter,i,KFCombo){
+    const tmp = KFCombo[0].indexOf(letter)
+    let match 
+    if (tmp == -1) {
+        match = 0
+    }
+    else if (tmp == i){
+        match = 2
+        KFCombo[0] = replaceAt(KFCombo[0],tmp,"*")
+    }
+    else {
+        match = 1
+        KFCombo[0] = replaceAt(KFCombo[0],tmp,"*")
+    }
+    KFCombo[1][i] = match
+    // console.log(`**** ${letter} appears at index ${tmp} in key ${KFCombo[0]}, match=${match}:${KFCombo[1]}`)
+    return KFCombo
+}
+
 function checkGuess(rowID,guess){
-    // feedback = [0,1,2,0,2]
-    feedback = [2,2,2,2,2]
+    const guessArr = Array.from(guess)
+    let KFCombo = [key,[0,0,0,0,0]]
+    guessArr.forEach((letter,i) => compareToKey(letter,i,KFCombo))
+    const feedback = KFCombo[1]
     // KEY: 0=wrong, 1=nearly, 2=exact
     const guessIDs = []
     Array.from(document.getElementById(rowID).childNodes).forEach(e => guessIDs.push(e.id))
-    console.log(guessIDs)
     guessIDs.forEach((id,i) => updateWordle(id, i, feedback[i]))
-    // console.log((prevVal,currVal) => prevVal + currVal, 0)
     const total = feedback.reduce((prevVal,currVal) => prevVal + currVal, 0)
-    if (total == 10) alert("You guessed!!")
+    if (total == 10) alert(`You guessed in ${currRow + 1}.`)
 }
 
 function keyDownHandler(e) {
     const testDiv = document.getElementById("test")
-    // keyPressed = e.key.charCodeAt(0)
-    // message = `key=${e.key} (${e.key.charCodeAt(0)}, ${e.key.length}),
-    //  code=${e.code}; shift key? ${e.shiftKey} [c=${currCol}/${maxCol},
-    //  r=${currRow}/${maxRow}]; at
-    //  EOR? ${atEOR}`
-    // testDiv.innerHTML = message 
     switch (e.code.slice(0,3)) {
         case "Key":
             if (!atEOR) document.getElementById(currCell()).innerHTML = e.key
@@ -84,13 +105,12 @@ function keyDownHandler(e) {
             break
         case "Ent":
             const rowID = `row${currRow}`
-            // console.log(`current line (id=${rowID}): ${document.getElementById(rowID).innerHTML}`)
             const guess = document.getElementById(rowID).textContent.replace(/\s+/g, '')
             if (guess.length != 5) {
                 alert("It needs to have five letters.")
                 break
             }
-            console.log(`current line (id=${rowID}): ${guess} [${guess.length} char(s)]`)
+            // console.log(`current line (id=${rowID}): ${guess} [${guess.length} char(s)]`)
             checkGuess(rowID,guess)
             incRow()
             currCol = 0
