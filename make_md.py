65fc5d67ef31e1bed97c9c73c449d41b0d2258c9
eccomaggio <eccomaggio@gmail.com>
2022-02-15 00:18:16 +0800

    added inline tags

--- make_md.py
+++ make_md.py
@@ -19,33 +19,52 @@ except:
 
 
 
-def checkList(line):
+def unorderedList(line,indent):
     para_type = f"list{line[0]}"
     return para_type
 
-def checkOList(line):
+def orderedList(line,indent):
     para_type = "olist"
     return para_type
 
-def checkHeading(line):
+def heading(line):
     count = re.compile("#+")
     depth = count.match(line).end()
     para_type = f"h{depth}"
     return para_type
 
-def checkBlockquote(line):
+def blockquote(line):
     para_type = "bquo"
     count = re.compile(">+")
     depth = count.match(line).end()
     return para_type
 
-def checkCode(line,to_close):
+def codeblock(line,to_close):
     para_type = "code"
     return para_type
 
-def checkPara(line):
+def paragraph(line, indent):
     para_type = "para"
     return para_type
+
+def inline_tags(line):
+    bold1 = re.compile('\*\*(.+?)\*\*')
+    bold2 = re.compile('__(.+?)__')
+    italic1 = re.compile('_(.+?)_')
+    italic2 = re.compile('\*(.+?)\*')
+    strikeout = re.compile('~~(.*?)~~')
+    links = re.compile('[^!]\[(.*?)\]\s*\((.*?)\)')
+    refs = re.compile('!\[(.*?)\]\((.*?)\)')
+
+    line = line.replace("<","&lt;").replace(">", "&gt;").replace("&","&amp;")
+    line = bold1.sub(r'<b>\1</b>', line)
+    line = bold2.sub(r'<b>\1</b>', line)
+    line = italic1.sub(r'<i>\1</i>', line)
+    line = italic2.sub(r'<i>\1</i>', line)
+    line = strikeout.sub(r'<s>\1</s>', line)
+    line = links.sub(r'<a href="\2">\1</a>', line)
+    line = refs.sub(r'<img src="\2" alt="\1" />', line)
+    return line
 
 
 if __name__ == "__main__":
@@ -59,6 +78,9 @@ if __name__ == "__main__":
             line = line.strip() ## Also removes the final newline  
             isBlank = True
             para_type = ""
+            initial_digit = re.compile(r'\d+\.')
+            initial_gt = re.compile(r'>+')
+            initial_hash = re.compile(r'#+')
 
             if line:
                 isBlank = False
@@ -66,26 +88,38 @@ if __name__ == "__main__":
                 if is_codeblock:
                     if line[0:3] == "```":
                         is_codeblock = False
-                        para_type = checkCode(line, False)
+                        para_type = codeblock(line, False)
+                        line = line[3:]
                     else:
-                        para_type = checkCode(line, True)
+                        para_type = codeblock(line, True)
                 elif line[0:3] == "```":
                     is_codeblock = True
-                    para_type = checkCode(line, True)
+                    para_type = codeblock(line, True)
+                    line = line[3:]
                 elif first_char in ("+", "-", "*"):
-                    para_type = checkList(line)
+                    para_type = unorderedList(line, indent)
+                    line = line[1:].lstrip()
                 elif first_char == "#":
-                    para_type = checkHeading(line)
+                    para_type = heading(line)
+                    line = initial_hash.sub("",line,1)
                 elif first_char == ">":
-                    para_type = checkBlockquote(line)
-                elif first_char.isnumeric():
-                    para_type = checkOList(line)
+                    para_type = blockquote(line)
+                    line = initial_gt.sub("",line,1)
+                # elif first_char.isnumeric():
+                elif initial_digit.match(line) is not None:
+                    para_type = orderedList(line, indent)
+                    line = initial_digit.sub("",line,1)
                 else:
-                    para_type = checkPara(line)
-                print(f"{para_type}: [{line[0]}]\t->{line}")
+                    para_type = paragraph(line, indent)
+                try:
+                    print(f"{para_type}: [{line[0]}]\t->{line}")
+                except:
+                    print(f"oops: {line}")
 
             else:
                 line = "&nbsp;"
+
+            line = inline_tags(line)
 
             tag_open, tag_close = "<p>","</p>"
             line = tag_open + line + tag_close + EOL
