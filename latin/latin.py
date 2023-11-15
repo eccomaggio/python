2f199af84fd6a9731489b450eb32115448da1435
eccomaggio <eccomaggio@gmail.com>
2023-11-15 13:14:10 +0000

    added line wrap, indent & colour display to notes; updated source files to accommodate.

--- latin/latin.py
+++ latin/latin.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 """
 add in row dividers where necessary
+
+json markup assumes:
+in notes:
+single quotes used for apostrophes
+double quotes used for quotes
+<...> signifies Latin
+
+in tables:
+: means 'macron'
+empty cells are marked by %
 """
 from pprint import pprint
 from dataclasses import dataclass
@@ -10,49 +20,50 @@ import getopt
 import json
 import re
 from tabulate import tabulate, SEPARATING_LINE
+
 tabulate.PRESERVE_WHITESPACE = True
 
+
 class col:
-    '''Colors class:reset all colors with colors.reset; two
+    """Colors class:reset all colors with colors.reset; two
     sub classes fg for foreground and bg for background; use as colors.subclass.colorname.
     i.e. colors.fg.red or colors.bg.green also, the generic bold, disable,
     underline, reverse, strike through, and invisible work with the main class i.e. colors.bold
-    '''
-    reset = '\033[0m'
-    bold = '\033[01m'
-    disable = '\033[02m'
-    underline = '\033[04m'
-    reverse = '\033[07m'
-    strikethrough = '\033[09m'
-    invisible = '\033[08m'
+    """
 
-    black = '\033[30m'
-    red = '\033[31m'
-    green = '\033[32m'
-    orange = '\033[33m'
-    blue = '\033[34m'
-    purple = '\033[35m'
-    cyan = '\033[36m'
-    lightgrey = '\033[37m'
-    darkgrey = '\033[90m'
-    lightred = '\033[91m'
-    lightgreen = '\033[92m'
-    yellow = '\033[93m'
-    lightblue = '\033[94m'
-    pink = '\033[95m'
-    lightcyan = '\033[96m'
+    reset = "\033[0m"
+    bold = "\033[01m"
+    disable = "\033[02m"
+    underline = "\033[04m"
+    reverse = "\033[07m"
+    strikethrough = "\033[09m"
+    invisible = "\033[08m"
+
+    black = "\033[30m"
+    red = "\033[31m"
+    green = "\033[32m"
+    orange = "\033[33m"
+    blue = "\033[34m"
+    purple = "\033[35m"
+    cyan = "\033[36m"
+    lightgrey = "\033[37m"
+    darkgrey = "\033[90m"
+    lightred = "\033[91m"
+    lightgreen = "\033[92m"
+    yellow = "\033[93m"
+    lightblue = "\033[94m"
+    pink = "\033[95m"
+    lightcyan = "\033[96m"
 
     class bg:
-        black = '\033[40m'
-        red = '\033[41m'
-        green = '\033[42m'
-        orange = '\033[43m'
-        blue = '\033[44m'
-        purple = '\033[45m'
-        cyan = '\033[46m'
-        lightgrey = '\033[47m'
-
-
+        black = "\033[40m"
+        red = "\033[41m"
+        green = "\033[42m"
+        orange = "\033[43m"
+        blue = "\033[44m"
+        purple = "\033[45m"
+        cyan = "\033[46m"
+        lightgrey = "\033[47m"
 
 
 class Db:
@@ -65,8 +76,6 @@ class Db:
         self.latin_macron = 1
         self.english = 2
         self.chapter = 3
-
-
 
 
 class Result:
@@ -82,7 +91,7 @@ class Result:
     def __repr__(self) -> str:
         return f"m:{self.matches}, {self.tables}\npm:{self.prev.matches}, {self.prev.tables}"
 
-    class Prev():
+    class Prev:
         def __init__(self):
             self.matches = []
             self.tables = []
@@ -125,12 +134,8 @@ class Result:
         return [x for x in matches if x.startswith("t")]
 
 
-
-
-
-
 def get_current_directory():
-    return Path( __file__ ).parent.absolute()
+    return Path(__file__).parent.absolute()
 
 
 def read_json(filename="latin.json"):
@@ -139,15 +144,6 @@ def read_json(filename="latin.json"):
         return json.load(f)
 
 
-
-def draw_table_title(title):
-    if title:
-        print(f"§ {col.bold}{col.orange}{title}{col.reset}")
-
-
-def draw_table_notes(notes):
-    for note in notes:
-        print(f"{col.lightblue}{note}{col.reset}")
 
 def draw_table(id, db):
     """
@@ -159,17 +155,17 @@ def draw_table(id, db):
     to allow display in either uk or us system
     """
     table = db.tables[id]
+    draw_table_title(table.get("title"))
+    draw_table_notes(table.get("notes"))
     tmp = "".join(table.get("data")[1]).strip()[:3].lower()
     if tmp in ["nom", "voc", "acc", "gen", "dat"]:
         table = convert_to_labelled_table(id, table, db)
         draw_labelled_table(id, table, db)
     else:
-        draw_table_title(table.get('title'))
-        draw_table_notes(table.get('notes'))
-        invoke_tabulate(table["data"])
+        invoke_tabulate(table.get("data"))
 
 
-def convert_to_labelled_table(id, table,db):
+def convert_to_labelled_table(id, table, db):
     tmp = table.copy()
     tmp["data"] = {}
     backup_label = "misc"
@@ -189,7 +185,7 @@ def convert_to_labelled_table(id, table,db):
             tmp["data"][label] = [row]
     return tmp
 
-# def draw_labelled_table(id, table, order="uk"):
+
 def draw_labelled_table(id, table, db):
     """
     In a labelled table, the ["data"] is a dictionary;
@@ -197,18 +193,33 @@ def draw_labelled_table(id, table, db):
     These can then be displayed in any order,
     i.e. UK (nom, voc, acc, ...) vs US (nom, gen, dat)
     """
-    # order = order.lower()
-    # if order != "uk":
-    #     order = "us"
     ordering = {
-        "uk": ["number", "gender", "nom", "voc", "acc", "gen", "par", "dat", "abl", "misc"],
-        "us": ["number", "gender", "nom", "gen", "par", "dat", "acc", "abl", "voc", "misc"],
+        "uk": [
+            "number",
+            "gender",
+            "nom",
+            "voc",
+            "acc",
+            "gen",
+            "par",
+            "dat",
+            "abl",
+            "misc",
+        ],
+        "us": [
+            "number",
+            "gender",
+            "nom",
+            "gen",
+            "par",
+            "dat",
+            "acc",
+            "abl",
+            "voc",
+            "misc",
+        ],
     }
-    # print(f"\nid: {id}")
-    draw_table_title(table.get('title'))
-    draw_table_notes(table.get('notes'))
     tmp = []
-    # for key in ordering[order]:
     for key in ordering[db.lang]:
         row = table["data"].get(key)
         if row:
@@ -218,9 +229,82 @@ def draw_labelled_table(id, table, db):
                 tmp.append(entry)
     invoke_tabulate(tmp)
 
+
 def invoke_tabulate(data):
-    print(tabulate(data, headers="firstrow", tablefmt="fancy_outline", colalign=("right",)))
+    print(
+        tabulate(
+            data, headers="firstrow", tablefmt="fancy_outline", colalign=("right",)
+        )
+    )
     # print(tabulate(data, headers="firstrow", tablefmt="fancy_outline")
+
+
+def draw_table_title(title):
+    if title:
+        print(f"§ {col.bold}{col.orange}{title}{col.reset}")
+
+
+def draw_table_notes(notes):
+    for note in notes:
+        for line in chunk_line(colour_pre(note), 70, 4, []):
+            print(f"{col.reset}{colour_final(line)}{col.reset}")
+
+
+def chunk_line(line, limit=70, indent=6, sublines=[]):
+    """
+    Takes a long string and then divides it into lines
+    at spaces (where possible) with a maximum line length.
+    Applies an indent to all but the first line.
+    """
+    ## Don't indent first line
+    end = limit
+    spacer = ""
+    if sublines:
+        end = limit - indent
+        spacer = " " * indent
+    if len(line) <= end:
+        sublines.append(spacer + line)
+        return sublines
+    subline = line[:end]
+    if subline[-1] != " ":
+        tmp = subline.rfind(" ")
+        if tmp > -1:
+            end = tmp
+            subline = line[:end]
+    sublines.append(spacer + subline)
+    line = line[end:].lstrip()
+    chunk_line(line, limit, indent, sublines)
+    return sublines
+
+
+def colour_pre(line):
+    """
+    This works along with colour_final() to convert simple markup
+    into colors: "..." is transformed into "{...}" in this first pass
+    To prevent the long command line colour codes disturbing
+    the line-length count, this translation from " to { or}
+    must be done before the lines are split in chunk_line()
+    """
+    quotes = line.split('"')
+    tmp = ""
+    for num, quote in enumerate(quotes):
+        sub = ["{", "}"] if num % 2 else ["", ""]
+        # print(num, num % 2, sub, sub[0] + quote + sub[1])
+        tmp += sub[0] + quote + sub[1]
+    return tmp
+
+
+def colour_final(line):
+    """
+    This continues on from colour_pre():
+    <...> is interpreted as one colour (Latin)
+    {...} is interpreted as another (often English translation)
+    """
+    line = line.replace('<', col.red)
+    line = line.replace('>', col.reset)
+    line = line.replace('{', col.green)
+    line = line.replace('}', col.reset)
+    return line
 
 
 def print_entry(entry, result, num):
@@ -235,11 +319,13 @@ def print_entry(entry, result, num):
     num_prefix = ""
     if num >= 0:
         num_prefix = f"{col.blue}{num + 1}. "
-    print(f"\t{num_prefix}{col.pink}{entry[lemma]} {col.reset}{col.bold}{entry[gloss]} {col.darkgrey}from chapter {col.lightblue}{entry[3]}{col.reset}")
+    print(
+        f"\t{num_prefix}{col.pink}{entry[lemma]} {col.reset}{col.bold}{entry[gloss]} {col.darkgrey}from chapter {col.lightblue}{entry[3]}{col.reset}"
+    )
 
 
 def search_db(term, language, db):
-    return [id for id,entry in db.vocab.items() if re.search(term, entry[language])]
+    return [id for id, entry in db.vocab.items() if re.search(term, entry[language])]
 
 
 def build_match_list(result):
@@ -273,16 +359,21 @@ def display_menu(result):
             if start_index < end_index:
                 menu_choices += f"*>* to *<*{end_index + 1}"
         extended_menu = f"{extended_menu}{menu_choices}*>*)"
-    msg = f"{base_menu}{extended_menu}*<*".replace("*<*", col.reset).replace("*>*", col.green)
+    msg = f"{base_menu}{extended_menu}*<*".replace("*<*", col.reset).replace(
+        "*>*", col.green
+    )
     print(msg)
     return menu_choices
+
 
 def check_numeric_entry(result, menu_choices):
     action = ""
     error = ""
     if result.prev.matches:
         id_in_prev = int(result.term) - 1
-        if 0 <= id_in_prev < len(result.prev.matches) and result.prev.matches[id_in_prev].startswith("t"):
+        if 0 <= id_in_prev < len(result.prev.matches) and result.prev.matches[
+            id_in_prev
+        ].startswith("t"):
             table_id = result.prev.matches[id_in_prev]
             action = f"display:{table_id}"
         else:
@@ -333,17 +424,16 @@ def display_results(result):
     else:
         result.show_error()
 
+
 def update_and_display(matches, result):
     result.update(matches)
     display_results(result)
     result.update([])
 
+
 def create_db(lang):
     return Db(
-        {
-            **read_json("wheelock_vocab.json"),
-            **read_json("table_entries.json")
-        },
+        {**read_json("wheelock_vocab.json"), **read_json("table_entries.json")},
         read_json("tables.json"),
         {
             "nom": "nom",
@@ -359,21 +449,25 @@ def create_db(lang):
             "sin": "number",
             "plu": "number",
         },
-        lang)
+        lang,
+    )
+
 
 def parse_cmd_line(argv):
     lang = ""
-    opts, _ = getopt.getopt(argv,"hl:",["help","lang=", "language="])
+    opts, _ = getopt.getopt(argv, "hl:", ["help", "lang=", "language="])
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print ("""
+            print(
+                """
 You can specify the following variables after pedigrees.py
     -h / --help=                <prints this help message>
     -l / --lang= / --language=  <us / uk style of listing noun cases>
                                 uk: nom, acc, gen, dat, abl
                                 us: nom, gen, dat, acc, abl
                                 e.g. -l us OR --lang=us
-""")
+"""
+            )
             sys.exit()
         elif opt in ("-l", "--lang", "--language"):
             lang = arg.lower()
@@ -389,7 +483,7 @@ def main(argv):
     while True:
         menu_choices = display_menu(result)
         result.term = input().strip()
-        match check_input(result,menu_choices):
+        match check_input(result, menu_choices):
             case "quit":
                 quit()
             case "continue":
@@ -407,8 +501,5 @@ def main(argv):
                 print("Houston, we have a problem...")
 
 
-
-
 if __name__ == "__main__":
     main(sys.argv[1:])
-
