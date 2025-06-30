041b75f6e74714abaeff16089b86ab865e6acbc0
eccomaggio <eccomaggio@gmail.com>
2025-06-30 22:57:01 +0100

    finessed the production of indicators; bug-fixes

--- art_cats/main.py
+++ art_cats/main.py
@@ -3,9 +3,7 @@ from dataclasses import dataclass
 from typing import List
 from enum import Enum, auto
 from pprint import pprint
-from datetime import datetime
-# import datetime
-
+from datetime import datetime, timezone
 
 @dataclass
 class Record:
@@ -25,7 +23,7 @@ class Record:
     publisher: str
     pub_year: str
     copyright: str
-    extent:int
+    extent: int
     size: int
     notes: str
     sales_code: str
@@ -39,21 +37,31 @@ class Record:
 
 
 def norm_langs(raw):
-    lang_list = { "english": "eng", "chinese": "chi", }
+    lang_list = {
+        "english": "eng",
+        "chinese": "chi",
+    }
     result = [lang_list[lang.strip().lower()] for lang in raw.split("/")]
     return result
 
+
 def norm_dates(raw):
-    result = [date.strip() for date in raw.split(",")  ]
+    result = [date.strip() for date in raw.split(",")]
     return result
+
+
+def normalize(entry):
+    return str(entry).strip() if entry else None
+
 
 def check_for_approx(raw):
     raw = raw.strip()
     is_approx = False
-    if (raw[-1] == "?"):
+    if raw[-1] == "?":
         is_approx = True
         raw = raw[:-1]
     return (raw, is_approx)
+
 
 def get_records(excel_file):
     workbook = load_workbook(filename=excel_file)
@@ -61,22 +69,29 @@ def get_records(excel_file):
     print(f"\n{sheet.title} in {excel_file}")
     current_time = datetime.now()
     records = []
-    # for value in sheet.iter_rows(min_row=2, max_row=2, values_only=True):
     for value in sheet.iter_rows(min_row=2, values_only=True):
-        if (not value[0]):
+        if not value[0]:
             break
         langs = norm_langs(value[1])
-        pub_date, pub_date_is_approx = check_for_approx(value[14])
+        pub_date, pub_date_is_approx = check_for_approx(str(value[14]))
         extent, extent_is_approx = check_for_approx(value[16])
-        # title_t = value[4]
-        # p_title_t = value[8]
-
         record = Record(
             value[0],
             langs,
-            *value[2:14],
+            normalize(value[2]),
+            normalize(value[3]),
+            normalize(value[4]),
+            normalize(value[5]),
+            normalize(value[6]),
+            normalize(value[7]),
+            normalize(value[8]),
+            normalize(value[9]),
+            normalize(value[10]),
+            normalize(value[11]),
+            normalize(value[12]),
+            normalize(value[13]),
             pub_date,
-            value[15],
+            normalize(value[15]),
             int(extent),
             normalize(value[17]),
             normalize(value[18]),
@@ -92,40 +107,36 @@ def get_records(excel_file):
         records.append(record)
     return records
 
-def normalize(entry):
-    return str(entry).strip() if entry else None
 
 def build_000():
     """leader (0 is only for sorting purposes; should read 'LDR')"""
-    return (0, ["00000nam a22000003i 4500"])
+    return build_field(0, [[-2,-2,"00000nam a22000003i 4500"]])
+
 
 def build_005(record):
     """date & time of transaction"""
-    standard_time = record.timestamp.utcnow()
-    # standard_time = datetime.now(datetime.UTC)
+    standard_time = record.timestamp.now(timezone.utc)
     timestamp = str(standard_time).translate(str.maketrans("", "", " -:"))[:16]
-    return (5, [f"{timestamp}"])
-    # return (5, [f"{record.timestamp}"])
+    return build_field(5, [[-2, -2, f"{timestamp}"]])
+
 
 def build_008(record):
     """pub year & main language?"""
     t = record.timestamp
-    return (
-        8,
-        [
-            f"{str(t.year)[2:]}{str(t.month).zfill(2)}{str(t.day).zfill(2)}s{record.pub_year}||||{record.place}||||||||||||||\\||eng||"
-        ],
-    )
+    return build_field( 8, [[-2,-2, f"{str(t.year)[2:]}{str(t.month).zfill(2)}{str(t.day).zfill(2)}s{record.pub_year}{4*"|"}{record.place}{14*"|"}\\||eng||" ]])
+
 
 def build_033(record):
     """sales dates"""
     count_of_dates = len(record.sale_dates)
     date_string = f"$a{"$a".join(record.sale_dates)}"
-    return (33, [f"{count_of_dates}\\{date_string}"])
+    return build_field(33, [[count_of_dates,-1, date_string]])
+
 
 def build_040():
     """catalogued in Oxford"""
-    return (40, ["\\\\$aUkOxU$beng$erda$cUkOxU"])
+    return build_field(40, [[-1,-1,"$aUkOxU$beng$erda$cUkOxU"]])
+
 
 def build_245(record):
     """
@@ -139,7 +150,7 @@ def build_245(record):
         title, subtitle = record.title, record.subtitle
         nonfiling, title = mark_nonfiling_words(title)
     title = combine(title, subtitle)
-    return (245, [f"0{nonfiling}$a{title}"])
+    return build_field(245, [[0,nonfiling, f"$a{title}"]])
 
 
 def build_264(record):
@@ -151,11 +162,12 @@ def build_264(record):
     if record.pub_year_is_approx:
         pub_year = f"[{pub_year}?]"
     pub_year = f",$c {pub_year}"
-    result.append(f"\\0{place}{publisher}{pub_year}")
+    result.append([-1, 0, f"{place}{publisher}{pub_year}"])
 
     if record.copyright:
-        result.append(f"$a\u00a9 {record.copyright}")
-    return (264, result)
+        result.append([-2,-2,f"$a\u00a9 {record.copyright}"])
+    return build_field(264, result)
+
 
 def build_300(record):
     """physical description"""
@@ -163,45 +175,59 @@ def build_300(record):
     if record.extent_is_approx:
         pages = f"approximately {pages}"
     size = f"{record.size} cm"
-    return (300, [f"\\$a{pages} ;$c{size}"])
+    return build_field(300, [[-1, -2, f"$a{pages} ;$c{size}"]])
+
 
 def build_336():
     """content type (boilerplate)"""
-    return (336, ["\\\\$atext$2rdacontent"])
+    return build_field(336, [[-1, -1, "$atext$2rdacontent"]])
+
 
 def build_337():
     """media type (boilerplate)"""
-    return (337, ["\\\\$aunmediated$2rdamedia"])
+    return build_field(337, [[-1, -1, "$aunmediated$2rdamedia"]])
+
 
 def build_338():
     """carrier type (boilerplate)"""
-    return (338, ["\\\\$avolume$2rdacarrier"])
+    return build_field(338, [[-1, -1, "$avolume$2rdacarrier"]])
+
 
 def build_876(record):
     """notes / donations / barcode"""
     notes = f"$z{record.notes}" if record.notes else ""
-    # hol_note = f"$z{record.hol_note}" if record.hol_note else ""
     donation = f"$z{record.donation}" if record.donation else ""
     barcode = f"$p{record.barcode}"
-    return (876, [f"\\\\{notes}{donation}{barcode}"])
+    return build_field(876, [[-1, -1, f"{notes}{donation}{barcode}"]])
+
 
 def build_904():
     """authority (boilerplate)"""
-    return (904, ["\\\\$aOxford Local Record"])
+    return build_field(904, [[-1, -1, "$aOxford Local Record"]])
 
-def build_024(record):	##optional
+
+def build_024(record):  ##optional
     """sales code (if exists)"""
-    sales_code = f"8\\$a{record.sales_code.strip()}" if record.sales_code else ""
-    return (24, [sales_code])
+    result = (
+        build_field(24, [[8, -1, f"$a{record.sales_code.strip()}"]])
+        if record.sales_code
+        else ""
+    )
+    return result
 
-def build_041(record):	##optional
+
+def build_041(record):  ##optional
     """language codes if not monolingual"""
-    lang_string = ""
-    if len(record.lang) > 1:
-        lang_string = f"\\0$a{"$a".join(record.lang)}"
-    return (41, [lang_string])
+    is_multi_lingual = len(record.lang) > 1
+    result = (
+        build_field(41, [[-1, 0, f"$a{"$a".join(record.lang)}"]])
+        if is_multi_lingual
+        else ""
+    )
+    return result
 
-def build_246(record):	##optional
+
+def build_246(record):  ##optional
     """
     Varying Form of Title
     Holds parallel Western title AND/OR original Chinese character title
@@ -223,40 +249,56 @@ def build_246(record):	##optional
         parallel_title = combine(record.p_title_t, record.p_title)
         parallel_subtitle = combine(record.p_subtitle_t, record.p_subtitle)
         nonfiling = 0
-    elif has_parallel_title:    ## (i.e. Western script)
+    elif has_parallel_title:  ## (i.e. Western script)
         parallel_title, parallel_subtitle = record.p_title, record.p_subtitle
         nonfiling, parallel_title = mark_nonfiling_words(parallel_title)
     else:
         parallel_title = ""
     if parallel_title:
-        parallel_title = combine(parallel_title,parallel_subtitle)
+        parallel_title = combine(parallel_title, parallel_subtitle)
 
     ## Combine Chinese character main title with Western parallel title (if either exists)
     ## (not sure how to handle Chinese original title + parallel titles)
     if has_chinese_main_title:
-        # chinese_character_title = "$b" + chinese_character_title
         result = combine(chinese_character_title, parallel_title)
     else:
         result = parallel_title
 
-    if result:
-        result = f"0{nonfiling}$a{result}"
+    result = build_field(246, [[0, nonfiling, f"$a{result}"]]) if result else ""
+    return result
 
-    return (246, [result])
 
-def build_500(record):	##optional
+def build_500(record):  ##optional
     """general notes"""
-    hol_note = f"\\\\$a{record.hol_note}" if record.hol_note else ""
-    return (500, [hol_note])
+    result = (
+        build_field(500, [[-1, -1, f"$a{record.hol_note}"]]) if record.hol_note else ""
+    )
+    return result
 
-def finesse_string(field):
-    """transforms e.g.
-    (40, ['blah']) -> (40, ['040=  blah'])
-    """
-    field_int = field[0]
-    field_designator = str(field[0]).zfill(3) if field[0] else "LDR"
-    field_contents = [f"{field_designator}=  {line}" for line in field[1]]
-    return (field_int, field_contents)
+
+def build_field(numeric_tag, data):
+    if data:
+        lines = []
+        display_tag = "LDR" if numeric_tag == 0 else str(numeric_tag).zfill(3)
+        line_start = f"{display_tag}=  "
+        for line in data:
+            i1 = expand_indicators(line[0])
+            i2 = expand_indicators(line[1])
+            lines.append(f"{line_start}{i1}{i2}{line[2]}")
+        result = (numeric_tag, lines)
+    else:
+        result = ""
+    return result
+
+
+def expand_indicators(indicator):
+    expansions = {-2: "", -1: "\\"}
+    try:
+        result = expansions[indicator]
+    except:
+        result = indicator
+    return result
+
 
 def mark_nonfiling_words(title):
     nonfiling = 0
@@ -267,10 +309,12 @@ def mark_nonfiling_words(title):
             title = test[0] + test[1]
     return (nonfiling, title)
 
+
 def combine(title, subtitle, sep="$b"):
     if subtitle:
         title += sep + subtitle
     return title
+
 
 def build_mark_records(records):
     mark_records = []
@@ -284,7 +328,6 @@ def build_mark_records(records):
             build_338,
             build_904,
         ):
-            # print(boilerplate())
             mark_record.append(boilerplate())
 
         for step in (
@@ -301,12 +344,10 @@ def build_mark_records(records):
             build_500,
         ):
             field = step(record)
-            if len(field[1][0]):
+            if field:
                 mark_record.append(field)
-            # mark_record.append(field)
 
         mark_record.sort(key=lambda field: field[0])
-        mark_record = [finesse_string(field) for field in mark_record]
         mark_records.append(mark_record)
 
     return mark_records
@@ -318,19 +359,15 @@ def main():
     records = get_records(excel_file)
     mark_records = build_mark_records(records)
 
-    # pprint(records)
-    with open('output.mrk', 'w') as f:
-        # f.write('readme')
+    with open("output.mrk", "w") as f:
         for record in mark_records:
             for field in record:
                 for line in field[1]:
                     # print(line)
                     f.write(line)
                     f.write("\n")
-            # print("")
             f.write("\n")
-    # pprint(mark_records)
-    pprint(records)
+    # pprint(records)
 
 
 if __name__ == "__main__":
