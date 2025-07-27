828a256169e65b6f685352c5412285ce0e8a23e4
eccomaggio <eccomaggio@gmail.com>
2025-07-27 10:16:04 +0100

    removed project 'art_cats' from this folder after moving it to independent repository

--- art_cats/main.py
+++
@@ -1,8 +0,0 @@
-from excel_to_marc21 import run
-
-
-def main() -> None:
-    run()
-
-if __name__ == "__main__":
-    main()
--- art_cats/test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from excel_to_marc21 import run
-
-class TestCalculations(unittest.TestCase):
-  pass
-
-if __name__ == "__main__":
-  unittest.main()
